b442a3be5763edc830a4d67a6f2a39572424c6a1
KsyTim <timoshenko.ksenia.1998@gmail.com>
2020-12-20 15:26:27 +0400

    create: added modal dialog

--- js/main.js
+++ js/main.js
@@ -91,7 +91,7 @@ $(document).ready(function () {
   // Optional parameters
     loop: true,
     autoplay: {
-        delay: 2000,
+        delay: 3000,
     },
     effect: 'fade',
     keyboard: {
@@ -116,4 +116,66 @@ $(document).ready(function () {
       readersSwiper.autoplay.start();
     }
   });
+
+    $(".subscribe-form").validate({
+    errorClass: "subscribe-invalid",
+    messages: {
+      'subscription-input': {
+        required: "Пожалуйста введите Ваш email",
+        email: "Необходимо ввести email в формате name@domain.com"
+      },
+    },
+  });
+  $(".modal-window-form").validate({
+    errorClass: "modal-invalid",
+    messages: {
+      'modal-accept': {
+        required: "Для того, чтобы отправить обращение, необходимо дать согласие на обработку персональных данных",
+      },
+      'direction': {
+        required: "Пожалуйста выберите тему обращения",
+      },
+      'input-email': {
+        required: "Пожалуйста введите Ваш email",
+        email: "Необходимо ввести email в формате name@domain.com"
+      },
+    },
+  });
+
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal-close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+  $(document).on('keydown', closeByButton);
+  $('.modal-overlay').on('click', closeModal);
+
+  function openModal(e) {
+    e.preventDefault();
+    var modalOverlay = $('.modal-overlay');
+    var modalWindow = $('.modal-window');
+    modalOverlay.addClass('modal-overlay--visible');
+    modalWindow.addClass('modal-window--visible');
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal-overlay');
+    var modalWindow = $('.modal-window');
+    modalOverlay.removeClass('modal-overlay--visible');
+    modalWindow.removeClass('modal-window--visible');
+  }
+
+  function closeByButton(e) {
+    var modalOverlay = $('.modal-overlay');
+    var modalWindow = $('.modal-window');
+    var key = e.which;
+
+    if (key == 27) {
+      modalOverlay.removeClass('modal-overlay--visible');
+      modalWindow.removeClass('modal-window--visible');
+    } else {
+      false;
+    }
+  }
+
 });
---
+++ send.php
@@ -0,0 +1,69 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+$subscription = $_POST['subscription-input'];
+$modalTitle = $_POST['direction'];
+$modalMessage = $_POST['modal-window-message'];
+$modalEmail = $_POST['input-email'];
+
+$titleSub = "Подписка Universal - веб разработка";
+$bodySub = "
+Данный пользователь <b>$subscription</b> хотел бы оформить новостную подписку!
+";
+
+$titleModal = "Новое обращение Universal - веб разработка";
+$bodyModal = "
+<h2>Новое обращение</h2>
+<b>Тема обращения:</b> $modalTitle<br>
+<b>E-mail:</b> $modalEmail<br><br>
+<b>Сообщение:</b><br>$modalMessage
+";
+
+function sendMessage($title,$body){
+    // Настройки PHPMailer
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    try {
+        $mail->isSMTP();   
+        $mail->CharSet = "UTF-8";
+        $mail->SMTPAuth   = true;
+        //$mail->SMTPDebug = 2;
+        $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+        // Настройки вашей почты
+        $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+        $mail->Username   = 'kseniia.timoshenko.0220@gmail.com'; // Логин на почте
+        $mail->Password   = '@choOl55'; // Пароль на почте
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port       = 465;
+        $mail->setFrom('kseniia.timoshenko.0220@gmail.com', 'Kseniia Timoshenko'); // Адрес самой почты и имя отправителя
+        // Получатель письма
+        $mail->addAddress('timoshenko.ksenia.1998@gmail.com');  
+        // Отправка сообщения
+        $mail->isHTML(true);
+        $mail->Subject = $title;
+        $mail->Body = $body;    
+
+        // Проверяем отравленность сообщения
+        if ($mail->send()) {$result = "success";} 
+        else {$result = "error";}
+
+    } catch (Exception $e) {
+        $result = "error";
+        $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+    }
+
+}
+
+$action = $_POST["action"];
+if ($action == "subscribe") {
+    sendMessage($titleSub, $bodySub);
+    header('Location: subscribe.php');
+} elseif ($action == "modal-window") {
+    sendMessage($titleModal, $bodyModal);
+    header('Location: booking.php');
+} else {
+    echo "Error";
+}
